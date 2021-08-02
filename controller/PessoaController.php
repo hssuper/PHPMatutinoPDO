1fff6daaa8a7b09eb8a8550f9c729a18ace4d72c
hssuper <higorsouza2306@gmail.com>
2021-08-02 09:17:36 -0300

    ad

--- controller/PessoaController.php
+++ controller/PessoaController.php
@@ -3,18 +3,57 @@ require_once "C:/xampp/htdocs/PHPMatutinoPDO/PHPMatutinoPDO/dao/daoPessoa.php";
 require_once 'C:/xampp/htdocs/PHPMatutinoPDO/PHPMatutinoPDO/model/Pessoa.php';
 class PessoController {
 
-    public function inserirPessoa($nome, $dtNasc, $login, $senha, 
-            $perfil, $email, $cpf){
-        $pessoa = new Pessoa();
-        $pessoa->setNome($nome);
-        $pessoa->setDtNasc($dtNasc);
-        $pessoa->setLogin($login);
-        $pessoa->setSenha($senha);
-        $pessoa->setPerfil($perfil);
+    public function inserirpessoa($nome, $dtNasc, 
+            $login, $senha, $perfil,$email,$cpf,$fkEndereco){
+        $pessoa = new pessoa();
+        $pessoa->setnome($nome);
+        $pessoa->setdtNasc($dtNasc);
+        $pessoa->setlogin($login);
+        $pessoa->setsenha($senha);
+        $pessoa->setperfil($perfil);
         $pessoa->setEmail($email);
-        $pessoa->setCpf($cpf);
-                
-        $daoPessoa = new daoPessoa();
-        return $daoPessoa->inserir($pessoa);
+        $pessoa->setcpf($cpf);
+        $pessoa->setfkEndereco($fkEndereco);
+        
+        $daopessoa = new Daopessoa();
+        return $daopessoa->inserir($pessoa);
+    }
+    
+    //método para atualizar dados de pessoa no BD
+    public function atualizarpessoa($id, $nome, $dtNasc, 
+            $login, $senha,$perfil){
+        $pessoa = new pessoa();
+        $pessoa->setIdpessoa($id);
+        $pessoa->setnome($nome);
+        $pessoa->setdtNasc($dtNasc);
+        $pessoa->setlogin($login);
+        $pessoa->setsenha($senha);
+        $pessoa->setperfil($perfil);
+        
+        $daopessoa = new Daopessoa();
+        return $daopessoa->atualizarpessoaDAO($pessoa);
+    }
+    
+    //método para carregar a lista de pessoas que vem da DAO
+    public function listarpessoas(){
+        $daopessoa = new Daopessoa();
+        return $daopessoa->listarpessoasDAO();
+    }
+    
+    //método para excluir pessoa
+    public function excluirpessoa($id){
+        $daopessoa = new Daopessoa();
+        return $daopessoa->excluirpessoaDAO($id);
+    }
+    
+    //método para retornar objeto pessoa com os dados do BD
+    public function pesquisarpessoaId($id){
+        $daopessoa = new Daopessoa();
+        return $daopessoa->pesquisarpessoaIdDAO($id);
+    }
+    
+    //método para limpar formulário
+    public function limpar(){
+        return $pr = new pessoa();
     }
 }
