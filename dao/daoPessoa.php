cf238b907637facd16e6ae8ed508c64ba582cc80
hssuper <higorsouza2306@gmail.com>
2021-08-02 08:57:14 -0300

    ad

--- dao/daoPessoa.php
+++ dao/daoPessoa.php
@@ -7,21 +7,183 @@ class daoPessoa {
 
     public $conn;
 
-    function inserir(Pessoa $p) {
-        $msg = "Dados cadastrados com sucesso!";
+    public function inserir(Pessoa $pessoa){
         $conn = new Conecta();
-        if ($conn->conectadb()) {
-            $sql = "insert into pessoa values (null,'" . $p->getNome() .
-                    "','" . $p->getDtNasc() . "','" . $p->getLogin() . "','" .
-                    $p->getSenha() . "','" . $p->getPerfil() . "','" .
-                    $p->getEmail() . "','" . $p->getCpf() . "')";
-            if(mysqli_query($conn->conectadb(), $sql) != 1){
-                $msg = "Erro de sintaxe. <br>";
+        $msg = new Mensagem();
+        $conecta = $conn->conectadb();
+        if($conecta){
+            $nome = $pessoa->getNome();
+            $dtNasc = $pessoa->getDtNasc();
+            $login = $pessoa->getLogin();
+            $senha = $pessoa->getSenha();
+            $perfil = $pessoa->getPerfil();
+            $email = $pessoa->getEmail();
+            $cpf = $pessoa->getCpf();
+           
+            try {
+                $stmt = $conecta->prepare("insert into pessoa values "
+                        . "(null,?,?,?,?,?,?,?,)");
+                $stmt->bindParam(1, $nome);
+                $stmt->bindParam(2, $dtNasc);
+                $stmt->bindParam(3, $login);
+                $stmt->bindParam(4, $senha);
+                $stmt->bindParam(5, $perfil);
+                $stmt->bindParam(6, $email);
+                $stmt->bindParam(7, $cpf);
+                
+                $stmt->execute();
+                $msg->setMsg("<p style='color: green;'>"
+                        . "Dados Cadastrados com sucesso</p>");
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
             }
-        } else
-            $msg = "Erro na conexão com Banco de Dados!";
-        mysqli_close($conn->conectadb());
+        }else{
+            $msg->setMsg("<p style='color: red;'>"
+                        . "Erro na conexão com o banco de dados.</p>");
+        }
+        $conn = null;
         return $msg;
+    }
+    
+    //método para atualizar dados da tabela pessoa
+    public function atualizarpessoaDAO(pessoa $pessoa){
+        $conn = new Conecta();
+        $msg = new Mensagem();
+        $conecta = $conn->conectadb();
+        if($conecta){
+            $idpessoa = $pessoa->getIdpessoa();
+            $nome = $pessoa->getNome();
+            $dtNasc = $pessoa->getLogin();
+            $senha = $pessoa->getSenha();
+            $perfil = $pessoa->getPerfil();
+            $email = $pessoa->getEmail();
+            $cpf = $pessoa->getCpf();
+           
+            try{
+                $stmt = $conecta->prepare("update pessoa set "
+                        . "nome = ?,"
+                        . "dtNasc = ?,"
+                        . "login = ?, "
+                        . "senha = ?, "
+                        . "perfil = ?, "
+                        . "email = ?, "
+                        . "cpf = ?, "
+                        . "where idpessoa = ?");
+                $stmt->bindParam(1, $nome);
+                $stmt->bindParam(2, $dtNasc);
+                $stmt->bindParam(3, $login);
+                $stmt->bindParam(4, $senha);
+                $stmt->bindParam(5, $perfil);
+                $stmt->bindParam(6, $email);
+                $stmt->bindParam(8, $cpf);
+                $stmt->bindParam(9, $idpessoa);
+                $stmt->execute();
+                $msg->setMsg("<p style='color: blue;'>"
+                        . "Dados atualizados com sucesso</p>");
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
+            }
+        }else{
+            $msg->setMsg("<p style='color: red;'>"
+                        . "Erro na conexão com o banco de dados.</p>");
+        }
+        $conn = null;
+        return $msg;
+    }
+    
+    //método para carregar lista de produtos do banco de dados
+    public function listarpessoasDAO(){
+        $conn = new Conecta();
+        $conecta = $conn->conectadb();
+        if($conecta){
+            try {
+                $rs = $conecta->query("select * from pessoa");
+                $lista = array();
+                $a = 0;
+                if($rs->execute()){
+                    if($rs->rowCount() > 0){
+                        while($linha = $rs->fetch(PDO::FETCH_OBJ)){
+                            $pessoa = new pessoa();
+                            $pessoa->setIdpessoa($linha->idpessoa);
+                            $pessoa->setNome($linha->nome);
+                            $pessoa->setDtNasc($linha->dtNasc);
+                            $pessoa->setLogin($linha->login);
+                            $pessoa->setSenha($linha->senha);
+                            $pessoa->setPerfil($linha->perfil);
+                            $pessoa->setEmail($linha->email);
+                            $pessoa->setCpf($linha->cpf);
+
+                            $lista[$a] = $pessoa;
+                            $a++;
+                        }
+                    }
+                }
+            } catch (Exception $ex) {
+                
+            }  
+            $conn = null;           
+            return $lista;
+        }
+    }
+    
+    //método para excluir produto na tabela produto
+    public function excluirpessoaDAO($id){
+        $conn = new Conecta();
+        $conecta = $conn->conectadb();
+        $msg = new Mensagem();
+        if($conecta){
+             try {
+                $stmt = $conecta->prepare("delete from pessoa "
+                        . "where idpessoa = ?");
+                $stmt->bindParam(1, $id);
+                $stmt->execute();
+                $msg->setMsg("<p style='color: #d6bc71;'>"
+                        . "Dados excluídos com sucesso.</p>");
+            } catch (Exception $ex) {
+                $msg->setMsg($ex);
+            }
+        }else{
+            $msg->setMsg("<p style='color: red;'>'Banco inoperante!'</p>"); 
+        }
+        $conn = null;
+        return $msg;
+    }
+    
+    //método para os dados de produto por id
+    public function pesquisarpessoaIdDAO($id){
+        $conn = new Conecta();
+        $conecta = $conn->conectadb();
+        $pessoa = new pessoa();
+        if($conecta){
+            try {
+                $rs = $conecta->prepare("select * from pessoa where "
+                        . "idpessoa = ?");
+                $rs->bindParam(1, $id);
+                if($rs->execute()){
+                    if($rs->rowCount() > 0){
+                        while($linha = $rs->fetch(PDO::FETCH_OBJ)){
+                            $pessoa->setIdpessoa($linha->idpessoa);
+                            $pessoa->setNome($linha->nome);
+                            $pessoa->setDtNasc($linha->dtNasc);
+                            $pessoa->setLogin($linha->login);
+                            $pessoa->setSenha($linha->senha);
+                            $pessoa->setPerfil($linha->perfil);
+                            $pessoa->setEmail($linha->email);
+                            $pessoa->setCpf($linha->cpf);
+                           
+                        }
+                    }
+                }
+            } catch (Exception $ex) {
+                
+            }  
+            $conn = null;
+        }else{
+            echo "<script>alert('Banco inoperante!')</script>";
+            echo "<META HTTP-EQUIV='REFRESH' CONTENT=\"0;
+			 URL='../PHPMatutinoPDO/cadastropessoa.php'\">"; 
+        }
+        return $pessoa;
     }
 
 }
