4f24279ced8fd7b1c6c100f3dc67134e95b34bf4
hssuper <higorsouza2306@gmail.com>
2021-08-03 08:24:22 -0300

    ad

--- model/Pessoa.php
+++ model/Pessoa.php
@@ -1,7 +1,7 @@
 <?php
 
 class Pessoa {
-    
+
     private $idpessoa;
     private $nome;
     private $dtNasc;
@@ -10,10 +10,16 @@ class Pessoa {
     private $perfil;
     private $email;
     private $cpf;
-    
+    private $fkendereco;
 
-    
-    
+    function getFkendereco() {
+        return $this->fkendereco;
+    }
+
+    function setFkendereco($fkendereco) {
+        $this->fkendereco = $fkendereco;
+    }
+
     function getIdpessoa() {
         return $this->idpessoa;
     }
@@ -77,7 +83,5 @@ class Pessoa {
     function setCpf($cpf) {
         $this->cpf = $cpf;
     }
-
-    
 
 }
