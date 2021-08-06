a7b85fccc820ec638d81b8b6e7b0b1f73dc23208
hssuper <higorsouza2306@gmail.com>
2021-08-06 09:11:48 -0300

    ad

--- controller/ProdutoController.php
+++ controller/ProdutoController.php
@@ -2,57 +2,79 @@
 include_once 'C:/xampp/htdocs/PHPMatutinoPDO/dao/DaoProduto.php';
 include_once 'C:/xampp/htdocs/PHPMatutinoPDO/model/Produto.php';
 
-class ProdutoController {
-    
-    public function inserirProduto($nomeProduto, $vlrCompra, 
-            $vlrVenda, $qtdEstoque, $fkfornecedor){
+class ProdutoController
+{
+
+    public function inserirProduto(
+        $nomeProduto,
+        $vlrCompra,
+        $vlrVenda,
+        $qtdEstoque,
+        $nomefornecedor,
+        $idfornecedor
+    ) {
+        $fornecedor = new Fornecedor();
+        $fornecedor->setNomeFornecedor($nomefornecedor);
+        $fornecedor->setIdfornecedor($idfornecedor);
+
+
         $produto = new Produto();
         $produto->setNomeProduto($nomeProduto);
         $produto->setVlrCompra($vlrCompra);
         $produto->setVlrVenda($vlrVenda);
         $produto->setQtdEstoque($qtdEstoque);
-        $produto->setFornecedor($fkfornecedor);
-        
-        
+
+
+
         $daoProduto = new DaoProduto();
         return $daoProduto->inserir($produto);
     }
-    
+
     //método para atualizar dados de produto no BD
-    public function atualizarProduto($id, $nomeProduto, $vlrCompra, 
-            $vlrVenda, $qtdEstoque, $fkfornecedor){
+    public function atualizarProduto(
+        $id,
+        $nomeProduto,
+        $vlrCompra,
+        $vlrVenda,
+        $qtdEstoque,
+        $fornecedor
+    ) {
         $produto = new Produto();
         $produto->setIdProduto($id);
         $produto->setNomeProduto($nomeProduto);
         $produto->setVlrCompra($vlrCompra);
         $produto->setVlrVenda($vlrVenda);
         $produto->setQtdEstoque($qtdEstoque);
-        $produto->setFornecedor($fkfornecedor);
-        
+        $produto->setFornecedor($fornecedor);
+
         $daoProduto = new DaoProduto();
         return $daoProduto->atualizarProdutoDAO($produto);
     }
-    
+
     //método para carregar a lista de produtos que vem da DAO
-    public function listarProdutos(){
+    public function listarProdutos()
+    {
         $daoProduto = new DaoProduto();
         return $daoProduto->listarProdutosDAO();
     }
-    
+
     //método para excluir produto
-    public function excluirProduto($id){
+    public function excluirProduto($id)
+    {
         $daoProduto = new DaoProduto();
         return $daoProduto->excluirProdutoDAO($id);
     }
-    
+
     //método para retornar objeto produto com os dados do BD
-    public function pesquisarProdutoId($id){
+    public function pesquisarProdutoId($id)
+    {
         $daoProduto = new DaoProduto();
         return $daoProduto->pesquisarProdutoIdDAO($id);
     }
-    
+
     //método para limpar formulário
-    public function limpar(){
+    public function limpar()
+    {
         return $pr = new Produto();
     }
 }
